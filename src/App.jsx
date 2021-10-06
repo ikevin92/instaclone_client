e86aaaeadc2b4f7ed1b2521c5ce40fc1edacf196
Kevin Orlando Echeverri Loboa <kevin.echeverri@pragma.com.co>
2021-10-06 09:44:05 -0500

    estilos globales sass

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'semantic-ui-react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>instaclone</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </div>
+  );
+};
+
+export default App;
